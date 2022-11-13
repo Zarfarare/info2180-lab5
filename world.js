67271ca9cc8cdc55f548ad4e6f37b669f74901f7
Fitzdaini Dinham <kemonte456@gmail.com>
2022-11-13 17:49:34 -0500

    Exercise 5

--- world.js
+++ world.js
@@ -1,10 +1,12 @@
 window.onload = function(){
     let LookUpbtn = document.querySelector("#lookup");
+    let Citylookup = document.querySelector("#lookupCity")
+    let result = document.querySelector("#result");
+    
     
     LookUpbtn.onclick = function(){
         let country = document.querySelector("#country").value;
-        let result = document.querySelector("#result");
-
+        
         fetch(`http://localhost/info2180-lab5/world.php?country=${country.trim()}`)
         .then(response => {
             return response.text();
@@ -15,5 +17,28 @@ window.onload = function(){
         .catch(err => {
             console.log(err);
         })
+    }
+
+
+    Citylookup.onclick = function(){
+        let country = document.querySelector("#country").value;
+
+        fetch(`http://localhost/info2180-lab5/world.php?country=${country.trim()}&lookup=cities`)
+        .then(response => {
+            return response.text();
+        })
+        .then(data => {
+            result.innerHTML = data;
+        })
+        .catch(err => {
+            console.log(err);
+        })
     };
+
+
+
+
 }
+
+
+
--- world.php
+++ world.php
@@ -14,11 +14,38 @@ $country = strtolower(filter_var($country, FILTER_SANITIZE_STRING));
 $lookup = filter_input(INPUT_GET, 'lookup', FILTER_SANITIZE_STRING);
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+if ($lookup == 'cities'){
+  $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities INNER JOIN countries ON cities.country_code=countries.code WHERE countries.name LIKE '%$country%'");
+}else{
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+
+<?php if($lookup == "cities"): ?>
+  <table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>District</th>
+            <th>Population</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($results as $row): ?>
+            <tr>
+                <td><?= $row['name']?></td>
+                <td><?= $row['district']?></td>
+                <td><?= $row['population']?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+  </table>
+ 
+<?php else: ?>
   <table>
     <thead>
         <tr>
@@ -39,3 +66,4 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <?php endforeach; ?>
     </tbody>
   </table>
+<?php endif; ?>
